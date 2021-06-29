fa28eda8257b63571f9891e8d81193968c65ff10
zxd882002 <zxd882002@126.com>
2021-06-29 17:37:26 +0800

    Add WeatherForecase API and Web

---
+++ src/WeatherForecastWeb/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using WeatherForecastWeb.Models;
+
+namespace WeatherForecastWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            IList<WeatherForecast> weatherForecasts = new List<WeatherForecast>();
+            weatherForecasts.Add(new WeatherForecast { Date = DateTime.Today, Summary = "Cold", TemperatureC = 0 });
+            weatherForecasts.Add(new WeatherForecast { Date = DateTime.Today.AddDays(1), Summary = "Cool", TemperatureC = 10 });
+            weatherForecasts.Add(new WeatherForecast { Date = DateTime.Today.AddDays(2), Summary = "Warm", TemperatureC = 20 });
+            weatherForecasts.Add(new WeatherForecast { Date = DateTime.Today.AddDays(3), Summary = "Hot", TemperatureC = 30 });
+
+            return View(weatherForecasts);
+        }
+    }
+}
