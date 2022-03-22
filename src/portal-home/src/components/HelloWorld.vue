3f9bb2d36ed391f5231ddaa8680474af013b35a9
zxd882002 <zxd882002@126.com>
2022-03-22 18:01:07 +0800

    test again

--- src/portal-home/src/components/HelloWorld.vue
+++ src/portal-home/src/components/HelloWorld.vue
@@ -1,41 +1,43 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>Weather Forcast ..........</p>
+    <p>Weather Forcast</p>
+    <button @click="get">Get ..........</button>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, onMounted } from "vue";
 import axios from "axios";
 
-export default defineComponent({
+export default {
   name: "HelloWorld",
   props: {
     msg: String,
   },
-});
-
-onMounted(() => {
-  console.log("test: http://weather-forecast-api/api/weatherforecast");
-  axios
-    .get("http://weather-forecast-api/api/weatherforecast")
-    .then((res) => {
-      console.log(res);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-  console.log("test: http://20.89.82.173/api/WeatherForecast");
-  axios
-    .get("http://20.89.82.173/api/WeatherForecast")
-    .then((res) => {
-      console.log(res);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-});
+  setup() {
+    var get = () => {
+      console.log("test: http://weather-forecast-api/api/weatherforecast");
+      axios
+        .get("http://weather-forecast-api/api/weatherforecast")
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+      console.log("test: http://20.89.82.173/api/WeatherForecast");
+      axios
+        .get("http://20.89.82.173/api/WeatherForecast")
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    };
+    return { get };
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
