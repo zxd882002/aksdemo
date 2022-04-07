13ac969c837f6930db457a4c2d7194cec8ef3d2a
zxd882002 <zxd882002@126.com>
2022-04-07 11:57:29 +0800

    try display the table

--- src/portal-home/src/components/HelloWorld.vue
+++ src/portal-home/src/components/HelloWorld.vue
@@ -3,20 +3,37 @@
     <h1>{{ msg }}</h1>
     <p>Weather Forcast</p>
     <button @click="get">Get /api/weatherforecast</button>
+    <table>
+      <thead>
+        <tr>
+          <th>Date</th>
+          <th>Summary</th>
+          <th>TemperatureC</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in weatherforecastDataReactive" :key="item.date">
+          <td>{{ item.date }}</td>
+          <td>{{ item.summary }}</td>
+          <td>{{ item.tmperatureC }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script lang="ts">
 import axios from "axios";
 import WeatherForecastData from "../models/WeatherForecastData";
+import { defineComponent, reactive } from "vue";
 
-export default {
+export default defineComponent({
   name: "HelloWorld",
   props: {
     msg: String,
   },
-  setup() {
-    let weatherforecastDatas: WeatherForecastData[] | undefined = undefined;
+  setup: () => {
+    let weatherforecastDatas: WeatherForecastData[] = [];
 
     var get = async () => {
       try {
@@ -30,9 +47,11 @@ export default {
       }
     };
 
-    return { get };
+    let weatherforecastDataReactive = reactive(weatherforecastDatas);
+
+    return { get, weatherforecastDataReactive };
   },
-};
+});
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
