9750cb846811c3809c5e413cc5bc41c3aabf5696
zxd882002 <zxd882002@126.com>
2022-04-08 13:16:16 +0800

    comlete left pannel, support icon

--- src/portal-home/src/App.vue
+++ src/portal-home/src/App.vue
@@ -3,7 +3,22 @@
     <el-container style="border: 1px solid #eee">
       <el-header class="header"> 淘淘の家</el-header>
       <el-container>
-        <el-aside width="200px" style="border: 1px solid #eee">Aside</el-aside>
+        <el-aside width="200px" style="border: 1px solid #eee">
+          <el-scrollbar
+            ><el-menu
+              ><el-sub-menu index="1">
+                <template #title>
+                  <coordinate
+                    style="width: 1em; height: 1em; padding-right: 1em;"
+                  /><span>小游戏</span>
+                </template>
+                <el-menu-item index="1-1">猜数字</el-menu-item>
+                <el-menu-item index="1-2">数独</el-menu-item>
+                <el-menu-item index="1-3">五子棋</el-menu-item>
+              </el-sub-menu>
+            </el-menu>
+          </el-scrollbar></el-aside
+        >
         <el-main style="border: 1px solid #eee"
           ><HelloWorld msg="Welcome to MY Vue.js + TypeScript App"
         /></el-main>
@@ -14,6 +29,7 @@
 
 <script setup lang="ts">
 import HelloWorld from "./components/HelloWorld.vue";
+import { Coordinate } from "@element-plus/icons-vue";
 </script>
 
 <style scoped>
@@ -21,7 +37,9 @@ import HelloWorld from "./components/HelloWorld.vue";
   background: #409eff;
   display: flex;
   align-items: center; /*垂直居中*/
-  height: 60px;
+  height: 3em;
   color: white;
+  font-size: larger;
+  font-weight: bold;
 }
 </style>
