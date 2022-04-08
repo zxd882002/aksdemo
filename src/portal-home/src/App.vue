24acff6a49d10ee1d42b8f2c133705ba0f97db5c
zxd882002 <zxd882002@126.com>
2022-04-08 11:38:39 +0800

    check in header style

--- src/portal-home/src/App.vue
+++ src/portal-home/src/App.vue
@@ -1,19 +1,27 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to MY Vue.js + TypeScript App" />
+  <div class="common-layout">
+    <el-container style="border: 1px solid #eee">
+      <el-header class="header"> 淘淘の家</el-header>
+      <el-container>
+        <el-aside width="200px" style="border: 1px solid #eee">Aside</el-aside>
+        <el-main style="border: 1px solid #eee"
+          ><HelloWorld msg="Welcome to MY Vue.js + TypeScript App"
+        /></el-main>
+      </el-container>
+    </el-container>
+  </div>
 </template>
 
 <script setup lang="ts">
 import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.header {
+  background: #409eff;
+  display: flex;
+  align-items: center; /*垂直居中*/
+  height: 60px;
+  color: white;
 }
 </style>
--- src/portal-home/src/components/HelloWorld.vue
+++ src/portal-home/src/components/HelloWorld.vue
@@ -52,6 +52,13 @@ const { weatherDatas, show, errorMsg } = toRefs(state);
 </script>
 
 <style scoped>
+.hello {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 h3 {
   margin: 40px 0 0;
 }
