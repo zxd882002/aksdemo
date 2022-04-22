62ac7323e9c69bf18bccd888c66a0be2aeaf3399
zxd882002 <zxd882002@126.com>
2022-04-22 13:53:47 +0800

    optimize the code and UI. fix the bugs

--- src/portal-home/src/main.ts
+++ src/portal-home/src/main.ts
@@ -1,10 +1,13 @@
-import { createApp } from 'vue'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
-import App from './App.vue'
-import router from './router/index'
+import { createApp } from "vue";
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+import App from "./App.vue";
+import router from "./router/index";
 
-const app = createApp(App)
-app.use(ElementPlus)
-app.use(router)
-app.mount('#app')
+import Axios from "axios";
+Axios.defaults.baseURL = "http://localhost:15000";
+
+const app = createApp(App);
+app.use(ElementPlus);
+app.use(router);
+app.mount("#app");
